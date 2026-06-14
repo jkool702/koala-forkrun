8ff9f73aa036e71bda64baaafbec4a9eb7e35a21
jkool702 <BuddhaEnergy13542>
2026-06-14 13:41:08 -0400

    reorder all.sh

--- all.sh
+++ all.sh
@@ -2,7 +2,7 @@
 
 cd "$(dirname "$0")" || exit 1
 
-benchmarks="oneliners bio vps-audit file-enc log-analysis web-index media-conv covid-mts sklearn nlp aurpkg unix50 riker max-temp"
+benchmarks="oneliners bio vps-audit file-enc log-analysis web-index media-conv covid-mts nlp aurpkg unix50 riker max-temp sklearn"
 
 log() { echo -e "[*] $1"; }
 
